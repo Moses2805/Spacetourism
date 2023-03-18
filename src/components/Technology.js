54e76746524fa372732a10bf16c955755622398f
Moses2805 <68960155+Moses2805@users.noreply.github.com>
2023-03-18 18:19:17 +0400

    Update Technology.js

--- src/components/Technology.js
+++ src/components/Technology.js
@@ -33,7 +33,7 @@ const Technology = () => {
                                 <div className="infoVehicle">
                                     <h6>THE TERMINOLOGY...</h6>
                                     <h1>LAUNCH VEHICLE</h1>
-                                    <div className="col-md-12 col-11">
+                                    <div className="col-md-12 col-10">
                                         <p>A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
                                     </div
                                 </div>
@@ -63,7 +63,7 @@ const Technology = () => {
                                 <div className="infoVehicle">
                                     <h6>THE TERMINOLOGY...</h6>
                                     <h1>SPACEPORT</h1>
-                                    <div className="col-md-12 col-11">
+                                    <div className="col-md-12 col-10">
                                         <p>A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth's rotation for launch.</p>
                                     </div>
                                 </div>
@@ -97,7 +97,7 @@ const Technology = () => {
                                 <div className="infoVehicle">
                                     <h6>THE TERMINOLOGY...</h6>
                                     <h1>SPACE CAPSULE</h1>
-                                    <div className="col-md-12 col-11">
+                                    <div className="col-md-12 col-10">
                                         <p>A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.</p>
                                     </div>
                                 </div>
