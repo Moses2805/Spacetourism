562017a932263d4a63deb5e286753a781acc4ac5
Moses2805 <68960155+Moses2805@users.noreply.github.com>
2023-03-18 18:12:11 +0400

    Update Technology.js

--- src/components/Technology.js
+++ src/components/Technology.js
@@ -33,8 +33,9 @@ const Technology = () => {
                                 <div className="infoVehicle">
                                     <h6>THE TERMINOLOGY...</h6>
                                     <h1>LAUNCH VEHICLE</h1>
-                                    <p>A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
-
+                                    <div className="col-md-12 col-11">
+                                        <p>A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!</p>
+                                    </div
                                 </div>
                             </div>
 
@@ -62,8 +63,9 @@ const Technology = () => {
                                 <div className="infoVehicle">
                                     <h6>THE TERMINOLOGY...</h6>
                                     <h1>SPACEPORT</h1>
-                                    <p>A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth's rotation for launch.</p>
-
+                                    <div className="col-md-12 col-11">
+                                        <p>A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth's rotation for launch.</p>
+                                    </div>
                                 </div>
                             </div>
 
@@ -95,8 +97,9 @@ const Technology = () => {
                                 <div className="infoVehicle">
                                     <h6>THE TERMINOLOGY...</h6>
                                     <h1>SPACE CAPSULE</h1>
-                                    <p>A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.</p>
-
+                                    <div className="col-md-12 col-11">
+                                        <p>A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.</p>
+                                    </div>
                                 </div>
 
                             </div>
